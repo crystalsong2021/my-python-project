76fa616f456f3bdb1da338cffc332a645eaeacea
crystal <2022csong@gmail.com>
2023-01-12 23:44:56 -0800

    [inprogress] excel

--- spreadsheet/main.py
+++ spreadsheet/main.py
@@ -2,3 +2,13 @@ import openpyxl
 
 inv_file = openxyl.load_workbook("inventory.xlsx")
 product_list = inv_file["Sheet1"]
+
+products_per_supplier = {}
+total_value_per_supplier = {}
+product_under_10_inv = {}
+
+for product_row in range (2, product_list.max_row + 1):
+    supplier_name = product_list.cell(product_row, 4).value
+    inventory = product_list.cell(product_row, 2).value
+    price = product_list.cell(product_row, 1).value
+    inventory_price = product_list.cell(product_row, 5)
