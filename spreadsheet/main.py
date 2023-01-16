8ddc14c257d288b94edc06ebd29f6eeab44ecea7
crystal <2022csong@gmail.com>
2023-01-15 23:00:19 -0800

    excel

--- spreadsheet/main.py
+++ spreadsheet/main.py
@@ -7,8 +7,15 @@ products_per_supplier = {}
 total_value_per_supplier = {}
 product_under_10_inv = {}
 
-for product_row in range (2, product_list.max_row + 1):
+for product_row in range(2, product_list.max_row + 1):
     supplier_name = product_list.cell(product_row, 4).value
     inventory = product_list.cell(product_row, 2).value
     price = product_list.cell(product_row, 1).value
     inventory_price = product_list.cell(product_row, 5)
+
+    # calculation number of products per supplier
+    if supplier_name in products_per_supplier:
+        current_num_products = products_per_supplier.get(supplier_name)
+        products_per_supplier[supplier_name] = current_num_products + 1
+    else:
+        products_per_supplier[supplier_name] = 1
