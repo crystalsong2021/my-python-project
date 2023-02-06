14a7e4f928152c0307ed1a388ceedcdc9cd220f7
crystal <2022csong@gmail.com>
2023-02-05 20:49:31 -0800

    top k element

---
+++ Leetcode 75- 2023/Top K Frequent Elements.py
@@ -0,0 +1,53 @@
+"""
+Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+
+
+Example 1:
+
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+Example 2:
+
+Input: nums = [1], k = 1
+Output: [1]
+
+
+Constraints:
+
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104
+k is in the range [1, the number of unique elements in the array].
+It is guaranteed that the answer is unique.
+"""
+
+
+def topKFrequent(nums, k):
+    count = {}
+    freq = [[] for i in range(len(nums) + 1)]
+
+    # frequencey [[], [], [], [], [], [], [], [], [], [], []]
+
+    for n in nums:
+        count[n] = 1 + count.get(n, 0)
+
+    # count {1: 3, 2: 2, 3: 4, 4: 1}
+
+    for n, c in count.items():
+        freq[c].append(n)
+
+    # [[], [4], [2], [1], [3], [], [], [], [], [], []]
+
+    res = []
+    # going in decrement order starting from 0
+    for i in range(len(freq) - 1, 0, -1):
+        for n in freq[i]:
+            res.append(n)
+            if len(res) == k:
+                return res
+
+# bucket sort --- o(n)
+# heapify - 0(log n)
+
+result = topKFrequent([1,1,1,2,2,3], 2)
+print(result)
