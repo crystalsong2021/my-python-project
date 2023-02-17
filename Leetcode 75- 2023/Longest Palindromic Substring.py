3fddc3b88971c6780643d4860cd01b9b6c82f0f7
crystal <2022csong@gmail.com>
2023-02-16 20:48:22 -0800

    longestpalidrome

--- Leetcode 75- 2023/Longest Palindromic Substring.py
+++ Leetcode 75- 2023/Longest Palindromic Substring.py
@@ -8,18 +8,24 @@ Output: "bab"
 Explanation: "aba" is also a valid answer.
 
 """
+
+
 def longestPalindrome(s):
     res = ""
     for i in range(len(s)):
-        res = len(findPalidome(i, i, s)) > len(res)
-        findPalidome(i, i+1, s)
-
-
-def findPalidome(l, r, s, res):
-
-    while (l >=0 and r < len(s) and s[l] == s[r]):
-        if r-l+1 > len(res):
-            res = s.slice(l, r+1)
-        l+=1
-        r-=1
+        res1 = len(findPalidome(i, i, s)) > len(res)
+        res2 = findPalidome(i, i + 1, s)
     return res
+
+
+def findPalidome(l, r, s, result):
+    while l >= 0 and r < len(s) and s[l] == s[r]:
+        if r - l + 1 > len(result):
+            result = s.slice(l, r + 1)
+        l += 1
+        r -= 1
+    return result
+
+
+res = longestPalindrome('ababdee')
+print(res)
