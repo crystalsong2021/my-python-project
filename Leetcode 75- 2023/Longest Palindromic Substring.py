0e9cd17b8c7fec4a5a46d805e64f7099c05731f2
crystal <2022csong@gmail.com>
2023-02-16 20:39:28 -0800

    longestpalidrome

---
+++ Leetcode 75- 2023/Longest Palindromic Substring.py
@@ -0,0 +1,15 @@
+"""
+5.Longest Palindromic Substring
+Given a string s, return the longest palindromic substring in s.
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+
+"""
+def longestPalindrome(s):
+    res = ""
+
+def findPalidome(l, r, s, res):
+    while (l)
